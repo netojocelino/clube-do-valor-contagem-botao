f9e3419f570054bf14282922e55a3c8bc983e684
Jocelino Neto <netoj96@live.com>
2023-11-27 11:42:10 -0300

    :tada: First Commit

---
+++ cbd-buttons-click-counts.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Buttons Click Counts
+ * Plugin URI:  https://github.com/netojocelino/cbs-buttons-clicks-counts
+ * Version:     1.0
+ * Description: Count the clicks from a button with shortcode.
+ * Author:      Jocelino Neto
+ * Author URI:  https://linkedin.com/in/netojocelino/
+ * License:     GPL v2 or later
+ */
+
